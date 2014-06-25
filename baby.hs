fad829e183855efbbde28aafc38d85aa0f2aac92
Cameron Dykes <yellow5c@gmail.com>
2014-06-25 18:33:28 -0500

    Add boomBangs function

--- baby.hs
+++ baby.hs
@@ -5,3 +5,5 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else doubleMe x
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
