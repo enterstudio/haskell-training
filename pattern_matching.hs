38da2d5b5a0d8418fce067a2d123066ba4dcd763
Cameron Dykes <yellow5c@gmail.com>
2014-06-30 23:49:52 -0500

    Add first, second, and third functions for triples

--- pattern_matching.hs
+++ pattern_matching.hs
@@ -16,3 +16,12 @@ factorial n = n * factorial(n - 1)
 
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1,y1) (x2,y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x,_,_) = x
+
+second :: (a, b, c) -> b
+second (_,y,_) = y
+
+third :: (a, b, c) -> c
+third (_,_,z) = z
