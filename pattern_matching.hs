100947b56e7abfc5081f493136f2724f632e499d
Cameron Dykes <yellow5c@gmail.com>
2014-07-01 00:06:43 -0500

    Add custom sum function using pattern matching and recursion

--- pattern_matching.hs
+++ pattern_matching.hs
@@ -39,3 +39,7 @@ tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " 
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
