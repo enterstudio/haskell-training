c8c9484785dfb2013ce8f659b7a7866443c7ab0c
Cameron Dykes <yellow5c@gmail.com>
2014-07-01 22:57:49 -0500

    Update bmiTell to to define bmi values using pattern matching

--- guards.hs
+++ guards.hs
@@ -4,10 +4,8 @@ bmiTell weight height
   | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
   | bmi <= fat    = "You're fat! Lose some weight, fatty!"
   | otherwise     = "You're a whale, congratulations!"
-  where bmi    = weight / height ^ 2
-        skinny = 18.5
-        normal = 25.0
-        fat    = 30.0
+  where bmi = weight / height ^ 2
+        (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
 max' :: (Ord a) => a -> a -> a
 max' a b
