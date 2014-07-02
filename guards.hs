9e722cc792a77c958968b9e1ac000f80c293d72f
Cameron Dykes <yellow5c@gmail.com>
2014-07-01 22:48:21 -0500

    Add myCompare function

--- guards.hs
+++ guards.hs
@@ -9,3 +9,9 @@ max' :: (Ord a) => a -> a -> a
 max' a b
   | a > b     = a
   | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a > b     = GT
+  | a == b    = EQ
+  | otherwise = LT
